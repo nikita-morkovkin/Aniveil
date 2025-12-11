d5d0ce5ed19a35b40f960a092c94ab9b5adbf921
Nikita Morkovkin <nikitamorkovkinwork@gmail.com>
2025-12-11 15:16:50 +0500

    🎉 init(backend, frontend):
    Created prisma module, installed modules for all the project

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,36 @@
+import {
+  Global,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+
+@Global()
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const databaseUrl = process.env.DATABASE_URL;
+
+    if (!databaseUrl) {
+      throw new Error('DATABASE_URL is not set in environment variables');
+    }
+
+    const adapter: PrismaPg = new PrismaPg({
+      connectionString: databaseUrl,
+    });
+    super({ adapter });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
